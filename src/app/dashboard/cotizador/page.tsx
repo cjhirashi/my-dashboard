3df6eedf4997977c2a93b3f1e50690b1bd20ada8
Carlos Jimenez <cjhirashi@gmail.com>
2024-08-03 04:37:08 -0600

    Actualización del proyecto

---
+++ src/app/dashboard/cotizador/page.tsx
@@ -0,0 +1,7 @@
+export default function Cotizador() {
+    return (
+      <div>
+        <h1>Cotizador de sistema de control</h1>
+      </div>
+    );
+  }
---
+++ src/app/dashboard/herramientas/page.tsx
@@ -0,0 +1,7 @@
+export default function Herramientas() {
+    return (
+      <div>
+        <h1>Herramientas de Carlos Jiménez</h1>
+      </div>
+    );
+  }
